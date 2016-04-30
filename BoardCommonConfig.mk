c0564046d661659d83a0bcabe9325e83404397ad
rogersb11 <brettrogers11@gmail.com>
2016-04-30 03:37:46 -0400

    Move to cyanide

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -174,10 +174,10 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.exynos4
 # LPM Battery Percentage
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 
-# Toolchain Flags
-TARGET_VRTOXIN_ROM := 4.9
-TARGET_NDK_VRTOXIN_ROM := 4.9
-TARGET_VRTOXIN_ARM := 4.8
+# Cyanide Build Flags
+TARGET_CYANIDE_ROM := 4.9
+TARGET_NDK_CYANIDE_ROM := 4.9
+TARGET_CYANIDE_ARM := 4.8
 
 # inherit from the proprietary version
 -include vendor/samsung/smdk4412-common/BoardConfigVendor.mk
